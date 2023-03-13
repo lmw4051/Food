0dc25dc30fbfa90a38f617133ac141e4c1621d72
David Lee <mikeportnoy168@gmail.com>
2023-03-13 17:21:46 +0800

    feat: add explore screen

--- fooderlich/lib/home.dart
+++ fooderlich/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/screens/explore_screen.dart';
+import 'screens/explore_screen.dart';
 import 'components/card1.dart';
 import 'components/card2.dart';
 import 'components/card3.dart';
@@ -14,7 +16,7 @@ class HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    const Card1(),
+    ExploreScreen(),
     const Card2(),
     const Card3(),
   ];
