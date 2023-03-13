edf23b5163929f51d9a12f6f93a0793487483cf3
David Lee <mikeportnoy168@gmail.com>
2023-03-13 14:59:12 +0800

    feat: apply theme

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -11,16 +11,26 @@ class Fooderlich extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       home: Scaffold(
         // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
         // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking 👩‍🍳',
+            style: theme.textTheme.headline1,
+          ),
+        ),
       ),
     );
   }
