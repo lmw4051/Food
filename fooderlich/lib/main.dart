53f8593cb6c9a35c36a5181a0ece6fe6648d8b3d
David Lee <mikeportnoy168@gmail.com>
2023-03-13 14:51:49 +0800

    refactor: rename and remove unnecessary code

---
+++ fooderlich/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    // TODO: Apply Home widget
+    return MaterialApp(
+      // TODO: Add theme
+      title: 'Fooderlich',
+      home: Scaffold(
+        // TODO: Style the title
+        appBar: AppBar(title: const Text('Fooderlich')),
+        // TODO: Style the body text
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+      ),
+    );
+  }
+}
