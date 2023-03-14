ee7a2db7b5adeb5caede407afa0ac764305f5275
David Lee <mikeportnoy168@gmail.com>
2023-03-14 21:34:21 +0800

    feat: add buildNameField widget

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -23,13 +23,108 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: Add GroceryItemScreen Scaffold
-    return Container(color: Colors.orange);
+  void initState() {
+    super.initState();
+    final originalItem = widget.originalItem;
+
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSliderValue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(
+        hour: date.hour,
+        minute: date.minute,
+      );
+      _dueDate = date;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
   }
 
-  // TODO: Add buildNameField()
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              // TODO: Add callback handler
+            },
+          )
+        ],
+        elevation: 0.0,
+        title: Text(
+          'Grocery Item',
+          style: GoogleFonts.lato(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            buildNameField(),
+            // TODO: Add Importance selection
+            // TODO: Add date picker
+            // TODO: Add time picker
+            // TODO: Add color picker
+            // TODO: Add slider
+            // TODO: Add Grocery Tile
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Item Name',
+          style: GoogleFonts.lato(fontSize: 28.0),
+        ),
+        TextField(
+          controller: _nameController,
+          cursorColor: _currentColor,
+          decoration: InputDecoration(
+            hintText: 'E.g. Apples, Banana, 1 Bag of salt',
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: _currentColor),
+            ),
+            border: UnderlineInputBorder(
+              borderSide: BorderSide(color: _currentColor),
+            ),
+          ),
+        )
+      ],
+    );
+  }
   // TODO: Add buildImportanceField()
   // TODO: Add builDateField()
   // TODO: Add buildTimeField()
